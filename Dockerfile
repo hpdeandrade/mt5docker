07ea8aeb9ef70ba59cbaf29374bb7a820ef6958a
Henrique Perez de Andrade <hpdeandrade@gmail.com>
2025-03-12 20:46:34 -0300

    Add interactive setup for MT5 through noVNC

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,12 @@
-FROM tobix/pywine:3.13
+FROM hpdeandrade/pywinedev:3.13
 
 WORKDIR /mt5docker
 
-# xvfb / x11vnc / novnc
 RUN apt update && \
     apt install xvfb x11vnc python3-websockify python3-numpy procps -y && \
-    curl -L -o noVNC.zip https://github.com/novnc/noVNC/archive/refs/heads/master.zip && \
-    unzip noVNC.zip && \
-    rm /mt5docker/noVNC.zip
+    curl -L -o noVNC.zip https://github.com/novnc/noVNC/archive/refs/heads/master.zip && unzip noVNC.zip && rm noVNC.zip && \
+    wine pip install MetaTrader5 pymt5linux
 
-# mt5
-ARG MT5_PATH="/opt/wineprefix/drive_c/Program Files/MetaTrader 5"
-COPY /mt5 ${MT5_PATH}
-COPY start.sh /mt5docker
-RUN curl -L -o mt5setup.exe https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe && \
-    wine mt5setup.exe && \
-    rm /mt5docker/mt5setup.exe && \
-    wine pip install MetaTrader5 && \
-    curl -L -o mt5linux.zip https://github.com/hpdeandrade/mt5linux/archive/refs/heads/master.zip && \
-    wine pip install mt5linux.zip && \
-    rm /mt5docker/mt5linux.zip
+COPY start.sh mt5cfg.ini /mt5docker/
 
-ENTRYPOINT ["sh", "./start.sh"]
+ENTRYPOINT ["./start.sh"]
--- start.sh
+++ start.sh
@@ -1,25 +1,45 @@
+#!/bin/bash
+
+cd /mt5docker
+
 # remove display lock if any
 rm -rf /tmp/.X100-lock
 
-# open mt5 terminal
+# set up display
 export DISPLAY=:100
 Xvfb :100 -ac -screen 0 1024x768x24 &
-cd "/opt/wineprefix/drive_c/Program Files/MetaTrader 5"
-wine terminal64.exe /config:custom.ini &
-
-# set up x11vnc
-cd /mt5docker
-x11vnc -storepasswd $VNC_PASSWORD /mt5docker/passwd
+x11vnc -storepasswd $VNC_PW /mt5docker/passwd
 x11vnc -display :100 -forever -rfbport 5901 -rfbauth /mt5docker/passwd &
 chmod 600 /mt5docker/passwd
+/mt5docker/noVNC-master/utils/novnc_proxy --vnc localhost:5901 --listen 6081 &
 
-# set up novnc
-cd /mt5docker/noVNC-master
-./utils/novnc_proxy --vnc localhost:5901 --listen 6081 &
+# install vc_redist_x86 if not installed yet
+if [ ! -f "/mt5docker/vc_redist.x86.exe" ]; then
+  curl -L -o vc_redist.x86.exe https://aka.ms/vs/17/release/vc_redist.x86.exe
+  wine vc_redist.x86.exe
+fi
 
-# set up mt5linux server
+# install vc_redist_x64 if not installed yet
+if [ ! -f "/mt5docker/vc_redist.x64.exe" ]; then
+  curl -L -o vc_redist.x64.exe https://aka.ms/vs/17/release/vc_redist.x64.exe
+  wine vc_redist.x64.exe
+fi
+
+# install mt5 if not installed yet
+if [ ! -f "/mt5docker/mt5setup.exe" ]; then
+  curl -L -o mt5setup.exe https://download.mql5.com/cdn/web/metaquotes.ltd/mt5/mt5setup.exe
+  wine mt5setup.exe
+  wine taskkill /IM "terminal64.exe" /F
+fi
+
+# open mt5
+mv "/mt5docker/mt5cfg.ini" "/opt/wineprefix/drive_c/Program Files/MetaTrader 5"
+cd "/opt/wineprefix/drive_c/Program Files/MetaTrader 5"
+wine terminal64.exe /config:mt5cfg.ini &
+
+# open mt5 linux
 cd /mt5docker
-wine python -m mt5linux --host $MT5_HOST --port 8001 C:/Python/python.exe
+wine python -m pymt5linux --host $MT5_HOST --port 8001 C:/Python/python.exe
 
 # prevent container termination
 while true
